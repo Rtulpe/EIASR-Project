867994037a951f46b1f475fb0ad2e580dbf6449c
Rustenis <r.tolpeznikas@gmail.com>
2025-01-16 21:23:34 +0100

    update main + test data

--- Delivery/main.py
+++ Delivery/main.py
@@ -1,18 +1,33 @@
 import os
 from ROI import RegionOfInterest
 from PROC import RectangleToTrapezoid, ProcessImage
-import cv2
-import numpy as np
+from OCR import OCR
+import glob
 
 root_dir = root_dir = os.path.abspath(os.path.dirname(__file__))
-test_img = os.path.join(root_dir, 'test.jpg')
+test_images = glob.glob(os.path.join(root_dir, 'Test_Data', '*.jpg'))
 
 roi = RegionOfInterest()
-images = roi.detect(test_img)
-coords = RectangleToTrapezoid(images[0])
-print(coords)
-out = ProcessImage(coords, images[0])
-for i in out:
-    cv2.imshow('image', i)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+ocr = OCR("FirstTest.mdl")
+
+for test_img in test_images[0]:
+
+    regions = roi.detect(test_img)
+    if not regions:
+        print("No license plate detected")
+        break
+
+    for r in regions:
+        coords = RectangleToTrapezoid(r)
+        print(f"Detected license plate coordinates: {coords}")
+        out = ProcessImage(coords, r)
+
+        if not out:
+            print(f"Error processing image: {test_img}")
+            break
+
+        number = []
+        for o in out:
+            number.append(ocr.predict(o))
+        
+        print(f"Detected license plate number: {''.join(number)}")
