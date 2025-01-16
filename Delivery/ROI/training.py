e8a87f48bf1813c27c4827c05061ded7dc3bda22
Rustenis <r.tolpeznikas@gmail.com>
2025-01-16 17:39:42 +0100

    Refactoring

--- Delivery/ROI/training.py
+++ Delivery/ROI/training.py
@@ -13,10 +13,10 @@ model_path = 'weights.pt'
 def train_model(model: YOLO = None):
     check_folders()
 
-    images_train_glob = get_globs('Training/images/*.png')
-    annot_train_glob = get_globs('Training/annotations/*.xml')
-    images_val_glob = get_globs('Validation/images/*.png')
-    annot_val_glob = get_globs('Validation/annotations/*.xml')
+    images_train_glob = get_globs('{}/images/*.png'.format(train_path))
+    annot_train_glob = get_globs('{}/annotations/*.xml'.format(train_path))
+    images_val_glob = get_globs('{}/images/*.png'.format(val_path))
+    annot_val_glob = get_globs('{}/annotations/*.xml'.format(val_path))
 
     if model is None:
         print("Model not found, fetching a new one...")
@@ -32,15 +32,15 @@ def train_model(model: YOLO = None):
     val_range = range(len(images_train_glob), len(images_train_glob) + len(images_val_glob))
 
     for i in train_range:
-        x1,y1,x2,y2,w,h = process_anns_file("Training/annotations/Cars{}.xml".format(i))
+        x1,y1,x2,y2,w,h = process_anns_file("{}/annotations/Cars{}.xml".format(train_path, i))
         x1,y1,w,h = bounding_box_in_yolo_format(x1,y1,x2,y2,w,h)
-        with open("Training/labels/Cars{}.txt".format(i), "w") as file:
+        with open("{}/labels/Cars{}.txt".format(train_path, i), "w") as file:
             file.write("0 {} {} {} {}".format(x1, y1, w, h))
 
     for i in val_range:
-        x1,y1,x2,y2,w,h = process_anns_file("Validation/annotations/Cars{}.xml".format(i))
+        x1,y1,x2,y2,w,h = process_anns_file("{}/annotations/Cars{}.xml".format(val_path, i))
         x1,y1,w,h = bounding_box_in_yolo_format(x1,y1,x2,y2,w,h)
-        with open("Validation/labels/Cars{}.txt".format(i), "w") as file:
+        with open("{}/labels/Cars{}.txt".format(val_path, i), "w") as file:
             file.write("0 {} {} {} {}".format(x1, y1, w, h))
 
     data = dict(
@@ -61,6 +61,10 @@ def check_folders():
         raise FileNotFoundError("Training folder not found")
     if not os.path.exists(val_path):
         raise FileNotFoundError("Validation folder not found")
+    if not os.path.exists(train_path + "/labels"):
+        os.makedirs(train_path + "/labels")
+    if not os.path.exists(val_path + "/labels"):
+        os.makedirs(val_path + "/labels")
     
 def get_globs(pattern):
     files = glob.glob(pattern)
